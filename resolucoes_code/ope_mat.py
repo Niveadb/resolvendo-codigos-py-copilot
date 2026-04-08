7fc38bdf9dd524758af296e36dae5f5b83fac15a
Nívea Domingues Batoni <nivea.batoni@unesp.br>
2026-04-08 22:08:04 +0000

    código operações matemática

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,29 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+# Recebendo os números do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))   
+
+# Recebendo a operação desejada
+operacao = input("Digite a operação desejada (+, -, *, /): ")
+
+# Realizando a operação escolhida
+if operacao == "+": #soma
+    resultado = num1 + num2
+
+elif operacao == "-": #subtração
+    resultado = abs(num1 - num2)
+
+elif operacao == "*": #multiplicação
+    resultado = num1 * num2
+
+elif operacao == "/": #divisão
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "Erro: Divisão por zero não é permitida." 
+else:
+    resultado = "Operação inválida."
+
+# Exibindo o resultado
+print("O resultado da operação é:", resultado)
